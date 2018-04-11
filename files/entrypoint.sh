3fe023f5233b722a6e08b2ca978530c66bacc80d
kos59125 <mail@recyclebin.jp>
2018-04-11 13:07:49 +0900

    wait Presto's startup finishes

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-launcher start
+launcher start >/dev/null 2>&1
+
+# wait until server is ready
+while true
+do
+   if [ -e /opt/presto/data/var/log/server.log ]
+   then
+      grep -q "SERVER STARTED" /opt/presto/data/var/log/server.log
+      if [ $? -eq 0 ]
+      then
+         break
+      fi
+   fi
+done
 
 presto --server localhost:8080 "$@"
